bd1ebc55dbfd2d04b512f0f5c940ef7428741a79
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-05-06 19:22:26 +0700

    session 6

---
+++ src/session6/Person.java
@@ -0,0 +1,14 @@
+package session6;
+
+public abstract class Person {
+    public String name;
+    public int age;
+
+    public void drive(){
+        System.out.println("driving...");
+    }
+
+    public abstract void drink();
+    public abstract void drink2();
+
+}
---
+++ src/session6/Student.java
@@ -0,0 +1,29 @@
+package session6;
+
+public class Student extends Person implements IPerson,IPerson2{
+
+    public void drink(){
+
+    }
+
+
+    public void drink2() {
+
+    }
+
+    public void dance(){
+
+    }
+
+    public void jump(){
+
+    }
+    public  void dance2(){
+
+    }
+
+    public int run(int x){
+        return 1;
+    }
+
+}
